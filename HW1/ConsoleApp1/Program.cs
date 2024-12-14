071e1279bdf471e30245c06fd3a71efd0d9a70a8
Jejcofa <bogdankuskov@gmail.com>
2024-12-14 13:59:12 +0200

    Added Program.cs from HW1

---
+++ HW1/ConsoleApp1/Program.cs
@@ -0,0 +1,63 @@
+﻿int order;
+double price;
+string client, product, address;
+
+// Order 1
+Console.WriteLine("Order No:");
+order = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Client name:");
+client = Console.ReadLine();
+Console.WriteLine("Product name:");
+product = Console.ReadLine();
+Console.WriteLine("Product price:");
+price = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Address:");
+address = Console.ReadLine();
+
+// Output 1
+Console.WriteLine("================");
+Console.WriteLine($"Order No {order}");
+Console.WriteLine($"Client: {client}.");
+Console.WriteLine($"Product: {product}, price {price} EUR.");
+Console.WriteLine($"Address: {address}.");
+Console.WriteLine("================");
+
+// Order 2
+Console.WriteLine("Order No:");
+order = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Client name:");
+client = Console.ReadLine();
+Console.WriteLine("Product name:");
+product = Console.ReadLine();
+Console.WriteLine("Product price:");
+price = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Address:");
+address = Console.ReadLine();
+
+// Output 2
+Console.WriteLine("================");
+Console.WriteLine($"Order No {order}");
+Console.WriteLine($"Client: {client}.");
+Console.WriteLine($"Product: {product}, price {price} EUR.");
+Console.WriteLine($"Address: {address}.");
+Console.WriteLine("================");
+
+// Order 3
+Console.WriteLine("Order No:");
+order = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Client name:");
+client = Console.ReadLine();
+Console.WriteLine("Product name:");
+product = Console.ReadLine();
+Console.WriteLine("Product price:");
+price = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Address:");
+address = Console.ReadLine();
+
+// Output 3
+Console.WriteLine("================");
+Console.WriteLine($"Order No {order}");
+Console.WriteLine($"Client: {client}.");
+Console.WriteLine($"Product: {product}, price {price} EUR.");
+Console.WriteLine($"Address: {address}.");
+Console.WriteLine("================");
